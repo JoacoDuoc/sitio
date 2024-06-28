61c6dd782071b6f2d3a0c4ffdc26edb4dbb1eae6
Italo121 <135144364+Italo121@users.noreply.github.com>
2024-06-28 07:43:15 -0400

    f

--- app/carrito.py
+++ app/carrito.py
@@ -14,15 +14,26 @@ class Carrito:
         if id not in self.carrito.keys():
             self.carrito[id]={
                 "producto_id": producto.id,
-                "nombre": producto.nombre,
-                "acumulado": producto.precio,
+                "nombre": producto.nombre_p,
+                "precio": producto.valor,
+                "acumulado": producto.valor,
                 "cantidad": 1,
             }
         else:
             self.carrito[id]["cantidad"] += 1
-            self.carrito[id]["acumulado"] += producto.precio
+            self.carrito[id]["acumulado"] += producto.valor
         self.guardar_carrito()
-
+        
+        
+    def sumar(self, producto):
+        id = str(producto.id)
+        if id in self.carrito.keys():
+            self.carrito[id]["cantidad"] += 1
+            self.carrito[id]["acumulado"] += producto.valor
+        self.guardar_carrito()     
+        
+        
+        
     def guardar_carrito(self):
         self.session["carrito"] = self.carrito
         self.session.modified = True
@@ -37,7 +48,7 @@ class Carrito:
         id = str(producto.id)
         if id in self.carrito.keys():
             self.carrito[id]["cantidad"] -= 1
-            self.carrito[id]["acumulado"] -= producto.precio
+            self.carrito[id]["acumulado"] -= producto.valor
             if self.carrito[id]["cantidad"] <= 0: self.eliminar(producto)
             self.guardar_carrito()
 
--- app/context_processor.py
+++ app/context_processor.py
@@ -1,7 +1,12 @@
 def total_carrito(request):
     total = 0
+    contador = 0
+    descuento = 0
     if request.user.is_authenticated:
         if "carrito" in request.session.keys():
             for key, value in request.session["carrito"].items():
                 total += int(value["acumulado"])
-    return {"total_carrito": total}
+                contador += int(value["cantidad"])
+    descuento = int(total * 0.93)
+    porcentaje = int( total * 0.07 )           
+    return {"total_carrito": total, "total_contador": contador, "descuento" : descuento, "porcentaje" : porcentaje}
